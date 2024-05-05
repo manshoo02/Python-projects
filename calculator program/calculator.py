b071df4a803a13118d0eb44addeee39e5269338e
manshoo02 <gulatimanshoo75@gmail.com>
2024-05-05 12:55:32 +0530

    Create calculator.py
    This Python project is a straightforward calculator application designed to perform basic arithmetic operations. It provides a user-friendly command-line interface for performing calculations quickly and efficiently.
    
    Key Features:
    
    1. Arithmetic Operations: Users can perform addition, subtraction, multiplication, and division operations on two numbers.
    2. Error Handling: The calculator handles invalid inputs gracefully, providing informative error messages to the user.
    3. User Interaction: The program prompts the user to input numbers and select an operation, making it intuitive to use.

---
+++ calculator program/calculator.py
@@ -0,0 +1,53 @@
+def add(n1,n2):
+    return n1 + n2
+
+def subtract(n1,n2):
+    return n1- n2
+
+def divide(n1,n2):
+    return n1 / n2
+
+def multiply(n1,n2):
+    return n1 * n2
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "/" : divide,
+    "*" : multiply
+    }
+
+def calculator():
+    num1 = float(input("Enter the first number : "))
+    for symbol in operations:
+        print(symbol)
+        
+    cont = True
+    
+    while cont:
+        op_symbol = input("Pick an operation : ")
+        if op_symbol not in ["+", "-" , "/","*"]:
+            print("Invalid operator!")
+            op_symbol = input("Pick an operation : ")
+
+        num2 = float(input("Enter the next number : "))
+        calc_function = operations[op_symbol]
+        ans = calc_function(num1,num2)
+        
+        print(f"{num1} {op_symbol} {num2} = {ans}")
+        
+        temp = input("Type 'y' to continue an operation with the answer , 'n' to start a new calculation and 'e' to exit the calculator program").lower()
+        if temp == "y":
+            num1 = ans
+        elif temp == "n":
+            cont = False
+            calculator()
+        elif temp == "e":
+            cont = False
+            print("Thanks for using the calculator program!")
+        else:
+            print("Invalid input! Read the instruction carefully")
+            temp = input("Type 'y' to continue an operation with the answer , 'n' to start a new calculation and 'e' to exit the calculator program").lower()
+            
+        
+calculator()   
