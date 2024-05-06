a845f01ff70524e17fb7ad372309c7c6884ec386
manshoo02 <gulatimanshoo75@gmail.com>
2024-05-06 22:04:50 +0530

    Guessing the number game
    Guessing the Number is a simple and entertaining Python script that challenges players to guess a randomly generated number within a specified range. With its intuitive interface and engaging gameplay, this game provides a fun way to test your guessing skills and luck.
    
    How to Play
    
    1. Objective: The goal of the game is to guess the randomly generated number within the given range.
    
    2. Gameplay: Players input their guesses via the command line, and the game provides feedback on whether the guess is too high, too low, or correct.
    
    3. Strategy: Players can use deduction and logic to narrow down the possible range of numbers based on the feedback provided.
    
    4. Winning: The game ends when the player correctly guesses the number or the attempts get exhausted.
    
    5. Difficulty Levels: Players have the option to adjust the difficulty level of the game.

---
+++ Guess the number Game/Guess_the_number.py
@@ -0,0 +1,63 @@
+import random
+logo ="""
+   _____                       _   _                                  _
+  / ____|                     | | | |                                | |
+ | |  __ _   _  ___  ___ ___  | |_| |__   ___   _ __  _   _ _ __ ___ | |__   ___ _ __
+ | | |_ | | | |/ _ \/ __/ __| | __| '_ \ / _ \ | '_ \| | | | '_ ` _ \| '_ \ / _ \ '__|
+ | |__| | |_| |  __/\__ \__ \ | |_| | | |  __/ | | | | |_| | | | | | | |_) |  __/ |
+  \_____|\__,_|\___||___/___/  \__|_| |_|\___| |_| |_|\__,_|_| |_| |_|_.__/ \___|_|
+
+
+"""
+print(logo)
+print("WELCOME TO THE NUMBER GUESSING GAME!")
+print("I'M THINKING OF A NUMBER BETWEEN 1 AND 100. Guess the number")
+num = random.randint(1,100)
+
+
+def guess_the_num(number):
+  game_over = False
+  diff = input("CHOOSE THE DIFFICULTY,. Type 'easy' or 'hard. ").lower()
+  if diff == "easy":
+    no_of_guess = 10
+    print("You have 10 attempts to guess the number. Good luck!")
+  elif diff == "hard":
+    no_of_guess = 5
+    print("You have 5 attempts to guess the number. Good luck!")
+  else:
+    ("INVALID INPUT!")
+  print(f"You have {no_of_guess} guesses remaining to guess the number.")
+
+  while not game_over and no_of_guess >0:
+    guess = int(input("Make a guess :"))
+    no_of_guess -=1
+    if guess > number:
+      print("Too high")
+    elif guess < number:
+      print("Too low")
+    else:
+      print("You got it right.")
+      game_over = True
+
+  if not game_over:
+    print(f"You failed to guess the number , the answer was {number} ")
+  start()
+
+def start():
+  x = input("DO YOU WANT TO START A NEW GAME? Type 'yes' or 'no'.").lower()
+  if x == 'yes':
+    guess_the_num(num)
+  elif x == 'no':
+    print("Thanks for playing the game!")
+  else:
+    print("INVALID INPUT!")
+    x = input("DO YOU WANT TO START A NEW GAME? Type 'yes' or 'no'.").lower()
+    if x == "yes":
+        guess_the_num(num)
+    elif x == "no":
+      print("Thanks for playing the game!")
+    else:
+      print("INVALID INPUT!")
+      start()
+
+start()
